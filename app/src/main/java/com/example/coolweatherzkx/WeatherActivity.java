9eff9f4c722b438b4eff3c1842d407babca026b0
Zkx <zkxvb@outlook.com>
2017-04-24 10:39:22 +0800

    新增切换城市和手动更新天气的功能订正

--- app/src/main/java/com/example/coolweatherzkx/WeatherActivity.java
+++ app/src/main/java/com/example/coolweatherzkx/WeatherActivity.java
@@ -146,6 +146,7 @@ public class WeatherActivity extends AppCompatActivity {
                                     getDefaultSharedPreferences(WeatherActivity.this).edit();
                             editor.putString("weather",responseText);
                             editor.apply();
+                            mWeatherId = weather.basic.weatherId; //更新天气Id
                             showWeatherInfo(weather); //显示天气信息
                         } else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败",Toast.LENGTH_SHORT).show();
