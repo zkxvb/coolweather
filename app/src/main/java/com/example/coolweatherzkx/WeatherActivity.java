d1ad3b99f525a9bcb2d18e43dab1a1e1b7ce3f33
Zkx <zkxvb@outlook.com>
2017-04-24 16:52:36 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/coolweatherzkx/WeatherActivity.java
+++ app/src/main/java/com/example/coolweatherzkx/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.coolweatherzkx;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -196,6 +197,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     /**
